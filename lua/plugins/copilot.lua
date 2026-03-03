362fb2ad47dae4e3dad69e947331a2d4d675df5e
Ganes556 <gustiganes@gmail.com>
2026-03-03 12:15:14 +0800

    feat(plugins): add and configure `copilot.lua` for inline suggestions

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,21 +1,30 @@
+-- lua/plugins/copilot.lua
 return {
+  -- copilot.lua handles inline suggestions only
   {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    dependencies = {
-      { "nvim-lua/plenary.nvim", branch = "master" },
-    },
-    build = "make tiktoken",
+    "zbirenbaum/copilot.lua",
     opts = {
+      filetypes = { templ = true, go = true, javascript = true },
       suggestion = {
         enabled = true,
         auto_trigger = true,
-        keymap = {
-          accept = "<S-Tab>",
-          next = "<M-]>",
-          prev = "<M-[>",
-          dismiss = "<C-]>",
-        },
+        keymap = { accept = "<S-Tab>" },
       },
+      -- do NOT put systemPrompt here, let CopilotChat handle it
     },
+  },
+
+  -- CopilotChat handles llms.txt as context
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    opts = function(_, opts)
+      local f = io.open(vim.fn.getcwd() .. "/docs/llms.txt", "r")
+      if f then
+        local content = f:read("*a")
+        f:close()
+        opts.system_prompt = "Use this project documentation as reference:\n\n" .. content
+      end
+      return opts
+    end,
   },
 }
